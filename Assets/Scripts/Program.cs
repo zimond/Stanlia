67803e7f801368023394a926e0ee74cc03dd1a37
daiheitan <daizhuoxian@gmail.com>
2014-12-13 20:09:55 +0800

    backquote to toggle console, add a player

--- Assets/Scripts/Program.cs
+++ Assets/Scripts/Program.cs
@@ -3,13 +3,20 @@ using System.Collections;
 using Stanlia;
 public class Program : MonoBehaviour {
 
+	private GameObject console;
+	private bool consoleVisibility = false;
 	// Use this for initialization
 	void Start () {
 		new Stanlia.Stanlia ();
+		console = GameObject.FindGameObjectWithTag ("Console");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//bind ` to the visibility of the console
+		if (Input.GetKeyDown(KeyCode.BackQuote)){
+			consoleVisibility = !consoleVisibility;
+		}
+		console.SetActive (consoleVisibility);
 	}
 }
